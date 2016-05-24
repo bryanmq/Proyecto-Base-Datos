9744d998fd7371a92829de3480d1de942785d7a7
Bryan <Bryan@BRYANMQ>
2016-05-24 15:18:32 -0600

    frmAcceso

--- prgProyectoBD/prgProyectoBD/frmAcceso.cs
+++ prgProyectoBD/prgProyectoBD/frmAcceso.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace prgProyectoBD
 {
-    public partial class Form1 : Form
+    public partial class frmAcceso : Form
     {
-        public Form1()
+        public frmAcceso()
         {
             InitializeComponent();
         }
