9c38647aa6da54520d361c3166cc8664e206db07
Bryan <Bryan@BRYANMQ>
2016-06-01 15:31:00 -0600

    Icons

---
+++ prgProyectoBD/prgProyectoBD/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Practica_1
+{
+    public partial class frmAgregarEstudiantes : Form
+    {
+      
+        public frmAgregarEstudiantes()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnEliminar(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnAgregar(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnActualizar(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnEditar(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void btnBuscar(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+        }
+    }
+}
