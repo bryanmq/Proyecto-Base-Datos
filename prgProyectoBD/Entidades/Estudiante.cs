e221c32e85d214f035031cb44f37c55fa9201c8c
Bryan <Bryan@BRYANMQ>
2016-06-02 22:24:17 -0600

    Sets Gets

--- prgProyectoBD/Entidades/Estudiante.cs
+++ prgProyectoBD/Entidades/Estudiante.cs
@@ -8,5 +8,83 @@ namespace Entidades
 {
     public class Estudiante
     {
+        #region Atributos
+        private String carnet, nombre, direccion, fechaNacimiento, telefono, email;
+        #endregion
+
+        public Estudiante()
+        {
+            this.carnet = "";
+            this.nombre = "";
+            this.direccion = "";
+            this.fechaNacimiento = "";
+            this.telefono = "";
+            this.email = "";
+        }
+        public Estudiante(String nombre, String direccion, String fechaNacimiento, String telefono, String email)
+        {
+
+        }
+
+        public void setCarnet(String carnet)
+        {
+            this.carnet = carnet;
+        }
+
+        public String getCarnet()
+        {
+            return this.carnet;
+        }
+
+        public void setNombre(String nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public String getNombre()
+        {
+            return this.nombre;
+        }
+
+        public void setDireccion(String direccion)
+        {
+            this.direccion = direccion;
+        }
+
+        public String getDireccion()
+        {
+            return this.direccion;
+        }
+
+        public void setFechaNac(String fechaNac)
+        {
+            this.fechaNacimiento = fechaNac;
+        }
+
+        public String getFechaNac()
+        {
+            return this.fechaNacimiento;
+        }
+
+        public void seTelefono(String telefono)
+        {
+            this.telefono = telefono;
+        }
+
+        public String getTelefono()
+        {
+            return this.telefono;
+        }
+
+        public void setEmail(String email)
+        {
+            this.email = email;
+        }
+
+        public String getEmail()
+        {
+            return this.email;
+        }
+
     }
 }
