6f2cb58a80fb0d494c4d60b2200814ac12a732e1
Bryan <Bryan@BRYANMQ>
2016-06-04 00:28:53 -0600

    Correción a la Entidad Fer

--- prgProyectoBD/prgProyectoBD/frmAcceso.Designer.cs
+++ prgProyectoBD/prgProyectoBD/frmAcceso.Designer.cs
@@ -130,7 +130,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(451, 168);
             this.ControlBox = false;
